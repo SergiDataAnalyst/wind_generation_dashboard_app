926bd0363ee670f923e6fa6852ec9238c2419c9e
Sergi <sergi.cmolina@gmail.com>
2023-12-06 11:39:03 +0100

    # shadowing variables bug fixing

--- test.py
+++ test.py
@@ -2,20 +2,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from mysql.connector import Error
 
 
 # Load data from CSV
 
 
-def process_uploaded_file(file_upload):
-    if file_upload is not None:
-        df = pd.read_csv(file_upload)
-        header = df.columns.tolist()
-        wind_speed_column = header[2]
-        power_output_column = header[1]
-        theoretical_power_column = header[3]
-        return df, wind_speed_column, power_output_column, theoretical_power_column
+def process_uploaded_file(csv_file):
+    if csv_file is not None:
+        uploaded_df = pd.read_csv(csv_file)
+        uploaded_header = uploaded_df.columns.tolist()
+        uploaded_wind_speed_column = uploaded_header[2]
+        uploaded_power_output_column = uploaded_header[1]
+        uploaded_theoretical_power_column = uploaded_header[3]
+        return uploaded_df, uploaded_wind_speed_column, uploaded_power_output_column, uploaded_theoretical_power_column
     else:
         print("Please upload a CSV file.")
 
@@ -29,6 +28,11 @@ tabs = ["Correlation Analysis", "Power Comparison", "Wind Rose"]
 selected_tab = st.sidebar.selectbox("Select Task", tabs)
 
 file_upload = st.sidebar.file_uploader("Upload CSV File", type=["csv"])
+
+wind_speed_column = None
+power_output_column = None
+header = None
+df = None
 
 # Load the data if the file is uploaded
 if file_upload is not None:
@@ -55,7 +59,8 @@ if selected_tab == "Correlation Analysis":
     st.subheader("Correlation between Wind Speed and Power Output")
     fig, ax = plt.subplots(figsize=(12, 8))
     fig.patch.set_facecolor('#282c34')
-    sns.regplot(x=wind_speed_column, y=power_output_column, data=df, scatter_kws={'alpha': 0.5, 'color': '#35B778'}, ax=ax)
+    sns.regplot(x=wind_speed_column, y=power_output_column,
+                data=df, scatter_kws={'alpha': 0.5, 'color': '#35B778'}, ax=ax)
     ax.tick_params(axis='x', colors='white')
     ax.tick_params(axis='y', colors='white')
     plt.title(f'Correlation Coefficient: {correlation_coefficient:.2f}', fontsize=16, color='white')
@@ -81,7 +86,9 @@ elif selected_tab == "Power Comparison":
     fig, ax = plt.subplots(figsize=(12, 8))
     fig.patch.set_facecolor('#282c34')
     sns.scatterplot(x=wind_speed_column, y=actual_power_column, data=df, label='Actual Power Output', alpha=0.5, ax=ax)
-    sns.lineplot(x=wind_speed_column, y=theoretical_power_column, data=df, label='Theoretical Power Curve', color='orange', ax=ax)
+    sns.lineplot(x=wind_speed_column, y=theoretical_power_column, data=df,
+                 label='Theoretical Power Curve', color='orange', ax=ax)
+
     ax.tick_params(axis='x', colors='white')
     ax.tick_params(axis='y', colors='white')
     plt.title('Comparison between Actual Power Output and Theoretical Power Curve', fontsize=16, color='white')
